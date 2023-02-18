0ee3aa2721145f382f6e5fffb6edd98240bdb603
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2023-02-19 01:21:38 +0500

    Initial Commit

---
+++ NewsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTestApp
+//
+//  Created by Dmitry Gorbunow on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
